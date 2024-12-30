8bb866bddd6cf45161c34f03c2b5b58a9cad5354
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-30 07:48:17 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      int count = 0;   // Prime number counter
+      int number = 1;  // Number to check if prime
+
+      // Instantiate an ArrayList.
+ 
+
+      // Find the first 100 prime numbers.
+
+
+      // Get a list iterator.
+
+      // Display the contents of the list.
+      
+   }
+
+   /**
+      The isPrime method determines whether a number is prime.
+      @param num The number to check.
+      @return true if the number is prime, false otherwise.
+   */
+
+   private static boolean isPrime(int num)
+   {
+
+   }
+}
